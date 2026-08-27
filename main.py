aed80e1742889d1406e551fcef0e153fb05c472b
MangoOnTop <tohfaeid@gmail.com>
2026-08-27 10:49:10 +0600

    update dispatch()

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from tools.calculator import calculator
+
 def dispatch(command : str) -> str:
-    
+    if command.startswith("calc "):
+        expression = command[5:]
+        return calculator(expression)
     return "no matching command found"
 
 def main():
     while True:
         command = input("Switchboard>>> ")
-        if command.lower()in ["exit", "quit"]:
+        if command.lower() in ["exit", "quit"]:
             break
         else:
             result = dispatch(command)
